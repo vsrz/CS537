775aa91f34866853ff76efe74a695c4d81136183
vsrz <ritual@vsritual.com>
2013-02-20 19:06:00 -0800

    Add examples

--- asg01/main.cpp
+++ asg01/main.cpp
@@ -9,21 +9,34 @@
 #include <iostream>
 #include <string>
 #include "netutils.hpp"
+#include "SocketServer.hpp"
 
 using namespace std;
 using namespace netutils;
 
+
 int main(int argc, char** argv) {
     string site;
     string res;
-
+/*
     if(argc != 2) {
         cout << "Usage: " << argv[0] << " <ip or hostname>" << endl;
         return 1;
     }
 
     resolveIpAddr(argv[1]);
+*/
+    try
+    {
+		SocketServer s(131,131,131);		
+    }
+    catch (SocketServerException e)
+    {
+		cout << e << endl;
+    }
 
+
+    
     return 0;
 
 }
