9e51ce047c60960f980932883e34b5f98471a1ff
vsrz <ritual@vsritual.com>
2013-03-19 21:59:19 -0700

    Send implemented

--- asg02/main.cpp
+++ asg02/main.cpp
@@ -22,27 +22,85 @@
 #include "fileutils.h"
 #include "dateutils.h"
 
+#define BUFFER_SIZE 8912
+#define DEFAULT_PORT 7777
+
 using namespace std;
 using namespace fileutils;
 
+struct sockaddr_in g_dest_host;
+
 void rdt_send(int fd, char *buf, size_t size)
 {
+	int res = 0;
 
+	/* put this shit on the wire and pray */
+	res = sendto(fd, buf, size + 1, 0, 
+		(struct sockaddr*) &g_dest_host, sizeof g_dest_host);
 
+	/* report any errors */
+	if( res < 0 )
+	{
+		/* what's the problem folks */
+		cout << strerror(errno) << "\n";
+	}
 }
 
 int rdt_recv(int fd, char *buf, size_t size)
 {
+	string fileContents;
+	// char *data;
+	int dg_socket;
+	struct sockaddr_in listen_host;
 
+	/* setup listen addr struct */
+	listen_host.sin_family = AF_INET;
+	listen_host.sin_port = htons(DEFAULT_PORT);
+	listen_host.sin_addr.s_addr = inet_addr("127.0.0.1");
 
+	/* bind socket and wait for conn */
+	dg_socket = socket(PF_INET, SOCK_STREAM, 0);
+	bind( dg_socket, (struct sockaddr*) &listen_host, sizeof listen_host);
+
+	return 0;
 }
 
-void sendFile(char *fn)
+void sendFile(char *fn, char *bind_addr = NULL, int bind_port = DEFAULT_PORT)
 {
 	string fileContents;
+	char *data;
+	int dg_socket;
+	struct sockaddr_in dest_host;
+
+	/* read data file into string */
 	readFile( fileContents, fn );
-	cout << fileContents << endl;
+
+	/* build destination addr struct */
+	if( bind_addr == NULL ) 
+	{
+		bind_addr = ( char* ) malloc(sizeof( char ) * 9);
+		strcpy( bind_addr,"127.0.0.1" );
+	}
+	dest_host.sin_family = NULL;
+	dest_host.sin_addr.s_addr = inet_addr(bind_addr);
+	dest_host.sin_port = htons(bind_port);
+
+	/* form the UDP socket */
+	dg_socket = socket( AF_INET, SOCK_DGRAM, 0 );
+	if( dg_socket < 0 ) 
+	{
+		cout << "Unable to create socket\n";
+		exit(1);
+	}
+
+	/* assign to global for now */
+	g_dest_host = dest_host;
+
+	/* convert string to char and send it into rdt_send */
+	data = (char *) malloc( sizeof( char )* fileContents.length() + 1);
+	strcpy ( data, fileContents.c_str());
 	
+	rdt_send(dg_socket, data, fileContents.length());
 }
 
 void recvFile(char *file)
